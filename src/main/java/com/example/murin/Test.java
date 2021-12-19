d004e1d4a8ecf2e9473be064597799b535a67f6e
DmitriiMurin <murin.dima@gmail.com>
2021-12-19 15:40:31 +0300

    MyArrayList

--- src/main/java/com/example/murin/Test.java
+++ src/main/java/com/example/murin/Test.java
@@ -1,7 +1,15 @@
 package com.example.murin;
 
+import com.example.murin.collections.list.MyArrayList;
+
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-
+        List<Integer> myArrayList = (List<Integer>) new MyArrayList<Object>(10);
+        for(int i = 0; i<10; i++){
+            myArrayList.add(i);
+            System.out.println(myArrayList);
+        }
     }
 }
