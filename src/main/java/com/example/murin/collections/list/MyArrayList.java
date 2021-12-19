9e4551ff3a80d435338ce323e73625a11b506dda
DmitriiMurin <murin.dima@gmail.com>
2021-12-19 15:34:34 +0300

    MyArrayList

--- src/main/java/com/example/murin/collections/list/MyArrayList.java
+++ src/main/java/com/example/murin/collections/list/MyArrayList.java
@@ -1,7 +1,63 @@
 package com.example.murin.collections.list;
 
-public class MyArrayList {
-//    forest on the fir
-//    test2
-    //test 3
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MyArrayList<E> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    transient Object[] elementData;
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    private int size;
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+    public MyArrayList (){
+        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+    }
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: "+
+                    initialCapacity);
+        }
+    }
+    public MyArrayList(Collection<? extends E> c) {
+        Object[] a = c.toArray();
+        if ((size = a.length) != 0) {
+            if (c.getClass() == ArrayList.class) {
+                elementData = a;
+            } else {
+                elementData = Arrays.copyOf(a, size, Object[].class);
+            }
+        } else {
+            // replace with empty array.
+            elementData = EMPTY_ELEMENTDATA;
+        }
+    }
+    private Object[] grow(int i) {
+        return grow(size + 1);
+    }
+
+    private void add(E e, Object[] elementData, int s) {
+        if (s == elementData.length)
+            elementData = grow(size + 1);
+        elementData[s] = e;
+        size = s + 1;
+    }
+
+    public void size(){
+
+    }
+
+    public void get(){
+
+    }
+
+    public void remove(){
+
+    }
+
 }
