403cab722012301e177ee012fc0414a996889b61
dkrylov <krylov.dmitry90@gmail.com>
2021-12-27 16:34:46 +0300

    - comments added - tests added

--- src/main/java/com/example/murin/collections/list/MyArrayList.java
+++ src/main/java/com/example/murin/collections/list/MyArrayList.java
@@ -3,13 +3,17 @@ package com.example.murin.collections.list;
 public class MyArrayList<E> {
 
     private int size;
+    //Use Object!
     private E[] array;
 
+    //create one more constructor, that takes initial size as an argument!
     public MyArrayList() {
         this.size = 10;
+        //Don't cast it here, use Object. Cast before get or other methods, which returns values!
         this.array = (E[]) new Object[size];
     }
 
+    //What will happen if I will add more than 10 elements.
     public boolean add(E e) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == null) {
@@ -19,15 +23,18 @@ public class MyArrayList<E> {
         }
         return false;
     }
-        public void size () {
 
-        }
+    //You should return int values, which is equals to your list current size!
+    public void size() {
 
-        public E get (int index) {
-        return array[index];
-        }
-
-        public void remove () {
-
-        }
     }
+    //What will happen, if you will get element with index higher than your list current size, but lower than your array current size??? You should throw exception.
+    public E get(int index) {
+        return array[index];
+    }
+
+    //implement it!
+    public void remove() {
+
+    }
+}
---
+++ src/test/java/com/example/murin/collections/list/MyArrayListTest.java
@@ -0,0 +1,54 @@
+package com.example.murin.collections.list;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class MyArrayListTest {
+
+    @Test
+    public void shouldAddSameValues() {
+        //create
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        ArrayList<Integer> correctArrayList = new ArrayList<>();
+        //fill
+        int limit = 100;
+        Random random = new Random();
+        IntStream.generate(random::nextInt).limit(limit).forEach(i -> {
+            myArrayList.add(i);
+            correctArrayList.add(i);
+        });
+        //check
+        Assertions.assertEquals(correctArrayList.size() == myArrayList.size());
+        Assertions.assertEquals(limit == myArrayList.size());
+        for(int i = 0; i < limit; i++) {
+            Assertions.assertEquals(correctArrayList.get(i), myArrayList.get(i));
+        }
+    }
+
+    @Test
+    public void shouldRemoveValueInTheMiddle() {
+        //create
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        ArrayList<Integer> correctArrayList = new ArrayList<>();
+        //fill
+        int limit = 100;
+        Random random = new Random();
+        IntStream.generate(random::nextInt).limit(limit).forEach(i -> {
+            myArrayList.add(i);
+            correctArrayList.add(i);
+        });
+        //remove
+        correctArrayList.remove(50);
+        myArrayList.remove(50);
+        //check
+        Assertions.assertEquals(correctArrayList.size() == myArrayList.size());
+        Assertions.assertEquals(limit == myArrayList.size());
+        for(int i = 0; i < limit; i++) {
+            Assertions.assertEquals(correctArrayList.get(i), myArrayList.get(i));
+        }
+    }
+}
